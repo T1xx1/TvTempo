f49b38e3ad828f579ea3927dbb5af5b453d382fe
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2026-07-10 13:47:02 +0200

    chore(app): init & setup

---
+++ apps/webapp/vite.config.ts
@@ -0,0 +1,27 @@
+import adapter from '@sveltejs/adapter-auto';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite-plus';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			alias: {
+				'~/*': './src/*',
+			},
+			compilerOptions: {
+				runes: ({ filename }) => {
+					return filename.split(/[/\\]/).includes('node_modules') ? undefined : true;
+				},
+				experimental: {
+					async: true,
+				},
+			},
+			adapter: adapter(),
+			experimental: {
+				remoteFunctions: true,
+			},
+		}),
+	],
+});
