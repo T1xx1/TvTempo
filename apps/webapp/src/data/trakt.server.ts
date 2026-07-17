9d86489c805ada64ca2382540b3c6f92500af061
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2026-07-17 13:09:01 +0200

    chore(trakt-sdk): rn

--- apps/webapp/src/data/trakt.server.ts
+++ apps/webapp/src/data/trakt.server.ts
@@ -1,6 +1,6 @@
 import { TRAKT_CLIENT_ID, TRAKT_CLIENT_SECRET } from '$env/static/private';
 import { PUBLIC_AUTH_REDIRECT_URI } from '$env/static/public';
-import { TraktClient } from '@tvtempo/trakt-api';
+import { TraktClient } from '@tvtempo/trakt-sdk';
 
 import { name, version } from '~/../package.json';
 
