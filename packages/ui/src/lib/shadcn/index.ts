de169393fbde6d99afdc4b02aa459763cf07cdef
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2026-07-23 13:51:34 +0200

    feat(ui): collapsible component

--- packages/ui/src/lib/shadcn/index.ts
+++ packages/ui/src/lib/shadcn/index.ts
@@ -86,6 +86,18 @@ export { Checkbox } from './checkbox/index.ts';
 
 /*  */
 
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './collapsible/index.ts';
+
+const CollapsibleComponents = {
+	Root: Collapsible,
+	Content: CollapsibleContent,
+	Trigger: CollapsibleTrigger,
+};
+
+export { CollapsibleComponents as Collapsible };
+
+/*  */
+
 import {
 	Drawer,
 	DrawerNestedRoot,
